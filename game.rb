e47cb56371cd2d9792fb23f8fcd47cba6cef6f07
cgehmlich <cgehmlich@gmail.com>
2018-12-04 18:05:24 +0000

    working version no player name

---
+++ game.rb
@@ -0,0 +1,52 @@
+require './questions'
+require './players'
+
+class Game
+
+    attr_reader :player1, :player2
+
+    def initialize (player1, player2)
+        @player1 = player1
+        @player2 = player2
+    end
+
+    def start_game
+        
+        while player1.health > 0 && player2.health > 0
+            question = Questions.new
+            puts "Player 1: #{question.question}"
+            if gets.chomp.to_i == question.answer
+                puts 'YES! You are correct.'
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+            else
+                puts 'WRONG!'
+                player1.health -= 1
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+            end
+
+            puts '----- NEW TURN -----'
+            question = Questions.new
+            puts "Player 2: #{question.question}"
+            if gets.chomp.to_i == question.answer
+                puts 'YES! You are correct.'
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+            else
+                puts 'WRONG!'
+                player2.health -= 1
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+            end
+            puts '----- NEW TURN -----'
+        end
+        
+        if player1.health > player2.health 
+            puts "Player 1 wins with a score of #{player1.health} 🎉"
+        elsif player1.health == player2.health
+            puts "Player 1 and Player 2 tied!"
+        else  
+            puts "Player 2 wins with a score of #{player2.health} 🎉"
+        end
+        puts '----- GAME OVER -----'
+        puts 'Good bye!'
+    end
+    
+end
---
+++ players.rb
@@ -0,0 +1,14 @@
+class Players 
+    
+        attr_accessor :health, :name
+        
+        def initialize
+            @health = 3 
+            @name = name
+        end
+        # def get_name
+        # puts "Enter player name"
+        # @name = gets.chomp
+        # end
+    end
+
